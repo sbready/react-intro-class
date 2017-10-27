371d7ede885acad1b12b498c74aed4e7e66453e1
Steven Bready <stevenbready@MacBook-Pro.local>
2017-10-27 15:29:22 -0600

    Completed Mini Project

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from './components/Calculator/Calculator.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+
+    );
+  }
+}
+
+export default App;
